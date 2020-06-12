99a4883c054af5eff4599d1b21d94b413edbd53c
unknown <tmdgh8512@hanmail.net>
2020-06-12 11:07:31 +0900

    update class2

---
+++ src/git_test/git_test2.java
@@ -0,0 +1,17 @@
+package git_test;
+
+import java.util.Scanner;
+
+public class git_test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner C = new Scanner(System.in);
+
+		int c = C.nextInt();
+		float f = (float) 9 / 5 * c + 32;
+		System.out.printf("%.1f", f);
+		
+	}
+
+}
